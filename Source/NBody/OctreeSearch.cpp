287ef61250abf89eebae2c2dfe26ff5c2ca84a29
Francisco García <fgarciaflorezmail@gmail.com>
2015-10-17 16:40:51 +0200

    Finished sequential implementation.

---
+++ Source/NBody/OctreeSearch.cpp
@@ -0,0 +1,97 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "NBody.h"
+#include "OctreeSearch.h"
+
+
+// Sets default values
+AOctreeSearch::AOctreeSearch() : Size(0), ParticleOctree(NULL), Initialized(false), ShowOctree(false), PhDeltaTime(0.01)
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AOctreeSearch::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void AOctreeSearch::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+  FlushPersistentDebugLines(GetWorld());
+  if (PhDeltaTime > 0) {
+    ComputeCubeSize();
+    CreateOctree();
+    for (int32 i = 0; i < Particles.Num(); i++) {
+      Particles[i].Velocity += PhDeltaTime*Particles[i].Acceleration;
+      Particles[i].Position += PhDeltaTime*Particles[i].Velocity;
+    }
+  }
+  DrawOctreeBoxes(ParticleOctree);
+}
+
+void AOctreeSearch::DrawOctreeBoxes(Octree* Oct)
+{
+  if (Oct == NULL) { return; }
+  if (Oct->IsLeafNode() && Oct->GetParticle()) {
+    if(ShowOctree) DrawDebugBox(GetWorld(), Oct->GetOrigin(), FVector(Oct->GetSize(), Oct->GetSize(), Oct->GetSize()), FColor::Red, true);
+    DrawDebugPoint(GetWorld(), Oct->GetParticle()->Position, 10.0, FColor::Black, true);
+  } else {
+    for (int32 i = 0; i < 8; i++) { DrawOctreeBoxes(Oct->GetChild(i)); }
+  }
+}
+
+void AOctreeSearch::ComputeCubeSize()
+{
+  if (!Initialized) return;
+  float t;
+  Size = Particles[0].Position.GetAbsMax();
+  for (int32 i = 1; i < Particles.Num(); i++) {
+    t = Particles[i].Position.GetAbsMax();
+    if (t > Size) { Size = t; }
+  }
+}
+
+void AOctreeSearch::CreateSpacePoints(int32 N, float SizeArg)
+{
+  Size = SizeArg;
+  FVector s = FVector(SizeArg, SizeArg, SizeArg/10);
+  Particles.SetNum(N);
+  for (int32 i = 0; i < N; i++) {
+    Particles[i].Position = FMath::RandPointInBox(FBox(GetActorLocation() - s, GetActorLocation() + s));
+    Particles[i].Velocity = 10*FMath::RandRange(25.0, 50.0)*FMath::VRand();
+    Particles[i].Mass = FMath::RandRange(1.0,5000.0);
+  }
+  Particles[0].Position = FVector::ZeroVector;
+  Particles[0].Velocity = FVector::ZeroVector;
+  Particles[0].Mass = 5000.0;
+  Initialized = true;
+}
+
+void AOctreeSearch::CreateOctree()
+{
+  if (!Initialized) return;
+  FVector t = FVector::ZeroVector;
+  if (ParticleOctree) { t = ParticleOctree->GetCenterOfMass(); delete ParticleOctree; }
+  ParticleOctree = new Octree(t, Size);
+  for (int32 i = 0; i < Particles.Num(); i++) { ParticleOctree->Add(&Particles[i]); }
+  ParticleOctree->ComputeMass();
+
+  for (int32 i = 0; i < Particles.Num(); i++) {
+    Particles[i].Acceleration = FVector::ZeroVector;
+    ParticleOctree->ComputeForces(&Particles[i], 1.0);
+  }
+
+  //DrawDebugBox(GetWorld(), ParticleOctree->GetCenterOfMass(), FVector(Size, Size, Size), FColor::Black, true);
+}
+
+void AOctreeSearch::CleanParticles()
+{
+  Initialized = false;
+  delete ParticleOctree;
+  ParticleOctree = NULL;
+  Particles.Empty();
+}
---
+++ Source/NBody/OctreeSearch.h
@@ -0,0 +1,149 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "OctreeSearch.generated.h"
+
+USTRUCT()
+struct FParticle {
+  GENERATED_USTRUCT_BODY()
+
+  float Mass;
+  FVector Position;
+  FVector Velocity;
+  FVector Acceleration;
+  
+  FParticle() : Mass(0), Position(FVector::ZeroVector), Velocity(FVector::ZeroVector), Acceleration(FVector::ZeroVector) {}
+};
+
+
+class Octree
+{
+private:
+  FParticle* Particle;
+  FVector Origin;
+  float Size;
+  float TotalMass;
+  FVector CenterOfMass;
+
+  Octree *Children[8];
+
+public:
+  Octree(const FVector& Origin, float Size) : Particle(NULL), Origin(Origin), Size(Size), TotalMass(0), CenterOfMass(FVector::ZeroVector) {
+    for (int32 i = 0; i < 8; i++) { Children[i] = NULL; }
+  }
+
+  ~Octree() {
+    if (!IsLeafNode()) {
+      for (int32 i = 0; i < 8; i++) { delete Children[i]; }
+    }
+  }
+
+  FParticle* GetParticle() const { return Particle; }
+  FVector GetOrigin() const { return Origin; }
+  float GetSize() const { return Size; }
+  float GetTotalMass() const { return TotalMass; }
+  FVector GetCenterOfMass() const { return CenterOfMass; }
+  Octree* GetChild(int32 i) { return Children[i]; }
+
+  int32 GetOctant(const FVector& Point) {
+    int32 octant = 0;
+    if (Point.X >= Origin.X) octant |= 4;
+    if (Point.Y >= Origin.Y) octant |= 2;
+    if (Point.Z >= Origin.Z) octant |= 1;
+    return octant;
+  }
+
+  bool IsLeafNode() const { return Children[0] == NULL; }
+
+  void Add(FParticle* Particle) {
+    if (IsLeafNode()) {
+      if (this->Particle == NULL) {
+        this->Particle = Particle;
+      } else {
+        FParticle* old = this->Particle;
+        this->Particle = NULL;
+
+        FVector center;
+        for (int32 i = 0; i < 8; i++) {
+          center = Origin;
+          center.X += Size * (i & 4 ? 0.5 : -0.5);
+          center.Y += Size * (i & 2 ? 0.5 : -0.5);
+          center.Z += Size * (i & 1 ? 0.5 : -0.5);
+          Children[i] = new Octree(center, 0.5*Size);
+        }
+
+        Children[GetOctant(old->Position)]->Add(old);
+        Children[GetOctant(Particle->Position)]->Add(Particle);
+      }
+    } else { Children[GetOctant(Particle->Position)]->Add(Particle); }
+  }
+
+  void ComputeMass() {
+    if (IsLeafNode()) {
+      if (Particle) {
+        CenterOfMass = Particle->Position;
+        TotalMass = Particle->Mass;
+      }
+    } else {
+      for (int32 i = 0; i < 8; i++) {
+        Children[i]->ComputeMass();
+        TotalMass += Children[i]->TotalMass;
+        CenterOfMass += Children[i]->TotalMass*Children[i]->CenterOfMass;
+      }
+      if (TotalMass) { CenterOfMass /= TotalMass; } else { CenterOfMass = Origin; }
+    }
+  }
+
+  void ComputeForces(FParticle* Particle, float Theta) {
+    if (IsLeafNode() && this->Particle == NULL) { return; }
+    float d = FVector::Dist(CenterOfMass, Particle->Position);
+    if (d == 0) { return; }
+    if (Size / d < Theta || this->Particle) {
+      Particle->Acceleration += 1e4*TotalMass/pow(d,3) * (CenterOfMass - Particle->Position);
+    } else if(!IsLeafNode()) {
+      for (int32 i = 0; i < 8; i++) { Children[i]->ComputeForces(Particle, Theta); }
+    }
+  }
+};
+
+UCLASS()
+class NBODY_API AOctreeSearch : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+  float Size;
+  TArray<FParticle> Particles;
+  Octree* ParticleOctree;
+
+  bool Initialized;
+
+  UPROPERTY(BlueprintReadWrite)
+  bool ShowOctree;
+
+  UPROPERTY(BlueprintReadWrite)
+  float PhDeltaTime;
+
+	// Sets default values for this actor's properties
+	AOctreeSearch();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+  void DrawOctreeBoxes(Octree* Oct);
+  void ComputeCubeSize();
+
+  UFUNCTION(BlueprintCallable, Category = "Octree")
+  void CreateSpacePoints(int32 N, float Size = 200);
+
+  UFUNCTION(BlueprintCallable, Category = "Octree")
+  void CreateOctree();
+
+  UFUNCTION(BlueprintCallable, Category = "Octree")
+  void CleanParticles();
+};
